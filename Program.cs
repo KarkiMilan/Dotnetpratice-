992d49ca5193dca9f8d64c108a2207160f46df95
karkimilan <Milankarki723@gmail.com>
2022-09-08 16:53:57 +0545

    Taking Input

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+int number1, number2;
+Console.WriteLine("Enter Your First Number");
+number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter Your Second Number");
+number2 = int.Parse(Console.ReadLine());
