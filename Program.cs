22a3b274ea10794f62bb764467439e693b8b85ba
karkimilan <Milankarki723@gmail.com>
2022-09-08 16:57:19 +0545

    Addition

--- Program.cs
+++ Program.cs
@@ -1,6 +1,31 @@
 ﻿using System;
-int number1, number2;
+int number1, number2, result, action;
 Console.WriteLine("Enter Your First Number");
 number1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Enter Your Second Number");
 number2 = int.Parse(Console.ReadLine());
+result = 0;
+Console.WriteLine("1 For Addition");
+action = int.Parse(Console.ReadLine());
+int Addition(int number1, int number2)
+{
+    int result = number1 + number2;
+    return result;
+}
+switch (action)
+{
+    case 1:
+        {
+            result = Addition(number1, number2);
+            break;
+        }
+    
+    default:
+        Console.WriteLine("Sorry Wrong Action");
+        break;
+
+}
+if (result != 0)
+{
+    Console.WriteLine("The result is {0}", result);
+}
